2685c79d811c87f2a1d33aae8d4182ff1c1c8bf8
Rishnu DK <rishnukrishnan@gmail.com>
2025-08-27 15:10:20 +0530

    Added env

--- src/Components/Firebase/Firebase.jsx
+++ src/Components/Firebase/Firebase.jsx
@@ -4,14 +4,13 @@ import { getStorage } from "firebase/storage";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCNswNfEe0QT_w2Cc_CZ37oihzQJSnUCUk",
-  authDomain: "olx-clone-99cf2.firebaseapp.com",
-  projectId: "olx-clone-99cf2",
-  storageBucket: "olx-clone-99cf2.firebasestorage.app",
-  messagingSenderId: "1073659323969",
-  appId: "1:1073659323969:web:e2bc670adb04dd042ec8c0"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
-
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
@@ -19,28 +18,20 @@ const provider = new GoogleAuthProvider();
 const storage = getStorage();
 const fireStore = getFirestore();
 
-
 const fetchFromFirestore = async () => {
-    try {
-      const productsCollection = collection(fireStore, 'products');
-      const productSnapshot = await getDocs(productsCollection);
-      const productList = productSnapshot.docs.map(doc => ({
-        id: doc.id,
-        ...doc.data(),
-      })) 
-      console.log("Fetched products from Firestore:", productList);
-      return productList;
-    } catch (error) {
-      console.error("Error fetching products from Firestore:", error);
-      return [];
-    }
-  };
-  
-
-  export {
-    auth,
-    provider,
-    storage,
-    fireStore,
-    fetchFromFirestore
+  try {
+    const productsCollection = collection(fireStore, "products");
+    const productSnapshot = await getDocs(productsCollection);
+    const productList = productSnapshot.docs.map((doc) => ({
+      id: doc.id,
+      ...doc.data(),
+    }));
+    console.log("Fetched products from Firestore:", productList);
+    return productList;
+  } catch (error) {
+    console.error("Error fetching products from Firestore:", error);
+    return [];
   }
+};
+
+export { auth, provider, storage, fireStore, fetchFromFirestore };
